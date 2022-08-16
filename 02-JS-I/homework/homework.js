c1dba5cfd77a60a3c31cc448ee1b096015f5b0a3
carlosdj12 <karlosdj510@gmail.com>
2022-08-16 01:03:27 -0300

    primer commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -60,7 +60,7 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  return x = y;
 }
 
 function tienenMismaLongitud(str1, str2) {
