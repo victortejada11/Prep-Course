4cdd5ed4fd6b7e4acf222d6600a07d471af2d130
carlosdj12 <karlosdj510@gmail.com>
2022-08-18 01:01:08 -0300

    segundo commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,32 +28,32 @@ const nuevoModulo = 21 % 5 === 1.5;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  str
+  return str;
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  
+  return x + y;
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  
+  return  x - y;
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-  
+  return x * y;
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  
+  return 
 }
 
 function sonIguales(x, y) {
